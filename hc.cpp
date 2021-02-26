8259447e2ab3f7d8d6c1e58abde8fbd7a3a3faf9
masterchief164 <rishugupta2001.rg@gmail.com>
2021-02-26 19:04:04 +0530

    comments completed till read function

--- hc.cpp
+++ hc.cpp
@@ -3,44 +3,44 @@
 #define ll long long
 using namespace std;
 
-class street//street class
+class street //street class
 {
 public:
-    long long length;//stores length of street
-    string name;//stores name of street
-    ll begid;//stores id of the intersection from where the street starts
-    ll endid;//stores id of the intersection from where the street ends
+    long long length; //stores length of street
+    string name;      //stores name of street
+    ll begid;         //stores id of the intersection from where the street starts
+    ll endid;         //stores id of the intersection from where the street ends
 };
 
-class intersection : public street//intersection class inherits street
+class intersection : public street //intersection class inherits street
 {
 public:
-    vector<street> incoming;//stores objects of all the incoming streets
-    vector<street> outgoing;//stores objects of all the outgoing streets
+    vector<street> incoming; //stores objects of all the incoming streets
+    vector<street> outgoing; //stores objects of all the outgoing streets
 };
 
-class cars : public street// cars class
+class cars : public street // cars class
 {
 public:
-    long long noofstreets;//stores the no of streets it passes through
-    vector<street> points;//stores the objects of all the streets it passes through
-    vector<string> streetname;//stores names of all the streets it passes through
+    long long noofstreets;     //stores the no of streets it passes through
+    vector<street> points;     //stores the objects of all the streets it passes through
+    vector<string> streetname; //stores names of all the streets it passes through
 };
 
 ll simtime, nointer, nostr, nocar, bp;
-unordered_map<string, street> streetmap;//map relates names of street to corresponding objects
-unordered_map<ll, intersection> intersectionmap;//map relates ids of intersection to corresponding intersections
-vector<cars> carsvec;//stores all the objects of cars
+unordered_map<string, street> streetmap;         //map relates names of street to corresponding objects
+unordered_map<ll, intersection> intersectionmap; //map relates ids of intersection to corresponding intersections
+vector<cars> carsvec;                            //stores all the objects of cars
 
-void readfile()// reads all the raw data and converts it into the objects
+void readfile() // reads all the raw data and converts it into the objects
 {
     string s;
-    ifstream filena("a.txt");//opens the file
-    ll c = 0;//counter variable
+    ifstream filena("a.txt"); //opens the file
+    ll c = 0;                 //counter variable
 
-    while (getline(filena, s))// reads the file line by line
+    while (getline(filena, s)) // reads the file line by line
     {
-        vector<string> ele;
+        vector<string> ele;// the entire part splits the line acc to space and stores the tokens in ele
         string delimiter = " ";
         auto start = 0U;
         auto end = s.find(delimiter);
@@ -50,40 +50,40 @@ void readfile()// reads all the raw data and converts it into the objects
             start = end + delimiter.length();
             end = s.find(delimiter, start);
         }
-        ele.push_back(s.substr(start, end));
-        if (c == 0)
+        ele.push_back(s.substr(start, end));// splitting complete
+        if (c == 0)// this block runs only one time
         {
-            simtime = stoi(ele[0]);
-            nointer = stoi(ele[1]);
-            nostr = stoi(ele[2]);
-            nocar = stoi(ele[3]);
-            bp = stoi(ele[4]);
+            simtime = stoi(ele[0]);// got the simulation time
+            nointer = stoi(ele[1]);// got the no of intersections
+            nostr = stoi(ele[2]);// got the no of streets
+            nocar = stoi(ele[3]);// got the no of cars
+            bp = stoi(ele[4]);// got the value of bonus points
             c = 1;
         }
-        else if (c <= nostr)
+        else if (c <= nostr)// this block runs nostr times
         {
-            street streetob;
-            streetob.begid = stoi(ele[0]);
-            streetob.endid = stoi(ele[1]);
-            streetob.name = ele[2];
-            streetob.length = stoi(ele[3]);
+            street streetob;// creating a street object
+            streetob.begid = stoi(ele[0]);// storing the id of the intersection from where the street starts
+            streetob.endid = stoi(ele[1]);// storing the id of the intersection from where the street ends
+            streetob.name = ele[2];//stores the street name
+            streetob.length = stoi(ele[3]);//stores the street length
             c++;
-            streetmap[streetob.name] = streetob;
+            streetmap[streetob.name] = streetob;// mapping object to street name
         }
         else
         {
-            cars carob;
-            ll nopath;
-            nopath = stoi(ele[0]);
+            cars carob;// creating a car object
+            ll nopath;// stores the no of streets the car passes through
+            nopath = stoi(ele[0]);// storing
             for (ll i = 0; i < nopath; i++)
             {
-                carob.streetname.push_back(ele[i + 1]);
-                carob.points.push_back(streetmap[ele[i + 1]]);
+                carob.streetname.push_back(ele[i + 1]);// storing the street names
+                carob.points.push_back(streetmap[ele[i + 1]]);// storing the corresponding street object
             }
-            carsvec.push_back(carob);
+            carsvec.push_back(carob);// storing the car object in a vector
         }
     }
-    filena.close();
+    filena.close();// file read complete
 }
 
 void process_data()
@@ -113,13 +113,17 @@ void process_data()
         obje.outgoing.push_back(x.second);
     }
 }
-void print_data(){
-    cout<<intersectionmap.size()<<"\n";
-    for(auto x:intersectionmap){
-        cout<<x.first<<"\n";
-        cout<<x.second.incoming.size()<<"\n";
-        for(auto k:x.second.incoming){
-            cout<<k.name<<" 1"<<"\n";
+void print_data()
+{
+    cout << intersectionmap.size() << "\n";
+    for (auto x : intersectionmap)
+    {
+        cout << x.first << "\n";
+        cout << x.second.incoming.size() << "\n";
+        for (auto k : x.second.incoming)
+        {
+            cout << k.name << " 1"
+                 << "\n";
         }
     }
 }
