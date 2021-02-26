47f3f28dc15e456efa80994a570bb2f381d75187
masterchief164 <rishugupta2001.rg@gmail.com>
2021-02-26 19:04:29 +0530

    reformatted the code

--- hc.cpp
+++ hc.cpp
@@ -40,7 +40,7 @@ void readfile() // reads all the raw data and converts it into the objects
 
     while (getline(filena, s)) // reads the file line by line
     {
-        vector<string> ele;// the entire part splits the line acc to space and stores the tokens in ele
+        vector<string> ele; // the entire part splits the line acc to space and stores the tokens in ele
         string delimiter = " ";
         auto start = 0U;
         auto end = s.find(delimiter);
@@ -50,40 +50,40 @@ void readfile() // reads all the raw data and converts it into the objects
             start = end + delimiter.length();
             end = s.find(delimiter, start);
         }
-        ele.push_back(s.substr(start, end));// splitting complete
-        if (c == 0)// this block runs only one time
+        ele.push_back(s.substr(start, end)); // splitting complete
+        if (c == 0)                          // this block runs only one time
         {
-            simtime = stoi(ele[0]);// got the simulation time
-            nointer = stoi(ele[1]);// got the no of intersections
-            nostr = stoi(ele[2]);// got the no of streets
-            nocar = stoi(ele[3]);// got the no of cars
-            bp = stoi(ele[4]);// got the value of bonus points
+            simtime = stoi(ele[0]); // got the simulation time
+            nointer = stoi(ele[1]); // got the no of intersections
+            nostr = stoi(ele[2]);   // got the no of streets
+            nocar = stoi(ele[3]);   // got the no of cars
+            bp = stoi(ele[4]);      // got the value of bonus points
             c = 1;
         }
-        else if (c <= nostr)// this block runs nostr times
+        else if (c <= nostr) // this block runs nostr times
         {
-            street streetob;// creating a street object
-            streetob.begid = stoi(ele[0]);// storing the id of the intersection from where the street starts
-            streetob.endid = stoi(ele[1]);// storing the id of the intersection from where the street ends
-            streetob.name = ele[2];//stores the street name
-            streetob.length = stoi(ele[3]);//stores the street length
+            street streetob;                // creating a street object
+            streetob.begid = stoi(ele[0]);  // storing the id of the intersection from where the street starts
+            streetob.endid = stoi(ele[1]);  // storing the id of the intersection from where the street ends
+            streetob.name = ele[2];         //stores the street name
+            streetob.length = stoi(ele[3]); //stores the street length
             c++;
-            streetmap[streetob.name] = streetob;// mapping object to street name
+            streetmap[streetob.name] = streetob; // mapping object to street name
         }
         else
         {
-            cars carob;// creating a car object
-            ll nopath;// stores the no of streets the car passes through
-            nopath = stoi(ele[0]);// storing
+            cars carob;            // creating a car object
+            ll nopath;             // stores the no of streets the car passes through
+            nopath = stoi(ele[0]); // storing
             for (ll i = 0; i < nopath; i++)
             {
-                carob.streetname.push_back(ele[i + 1]);// storing the street names
-                carob.points.push_back(streetmap[ele[i + 1]]);// storing the corresponding street object
+                carob.streetname.push_back(ele[i + 1]);        // storing the street names
+                carob.points.push_back(streetmap[ele[i + 1]]); // storing the corresponding street object
             }
-            carsvec.push_back(carob);// storing the car object in a vector
+            carsvec.push_back(carob); // storing the car object in a vector
         }
     }
-    filena.close();// file read complete
+    filena.close(); // file read complete
 }
 
 void process_data()
