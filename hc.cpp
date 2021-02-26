c55047fc4fb8a0e49bce32cb1d076adb05473240
masterchief164 <rishugupta2001.rg@gmail.com>
2021-02-26 17:43:45 +0530

    input done

--- hc.cpp
+++ hc.cpp
@@ -2,27 +2,35 @@
 #include <fstream>
 #define ll long long
 using namespace std;
-/*class street
+
+class street
 {
 public:
-    intersection *start;
-    intersection *end;
     long long length;
     string name;
+    ll begid;
+    ll endid;
 };
-class intersection
+
+class intersection : public street
 {
 public:
     vector<street> incoming;
     vector<street> outgoing;
 };
-class cars
+
+class cars : public street
 {
 public:
     long long noofstreets;
-    vector<intersection> points;
-};*/
+    vector<street> points;
+    vector<string> streetname;
+};
+
 ll simtime, nointer, nostr, nocar, bp;
+unordered_map<string, street> streetmap;
+vector<cars> carsvec;
+
 void readfile()
 {
     string s;
@@ -32,14 +40,16 @@ void readfile()
     while (getline(filena, s))
     {
         vector<string> ele;
+        string delimiter = " ";
         auto start = 0U;
-        auto end = s.find(" ");
-        while (end != string::npos)
+        auto end = s.find(delimiter);
+        while (end != std::string::npos)
         {
             ele.push_back(s.substr(start, end - start));
-            start = end + 1;
-            end = s.find(" ", start);
+            start = end + delimiter.length();
+            end = s.find(delimiter, start);
         }
+        ele.push_back(s.substr(start, end));
         if (c == 0)
         {
             simtime = stoi(ele[0]);
@@ -49,14 +59,37 @@ void readfile()
             bp = stoi(ele[4]);
             c = 1;
         }
-        else if (c <= nostr + 1)
+        else if (c <= nostr)
         {
-
+            street streetob;
+            streetob.begid = stoi(ele[0]);
+            streetob.endid = stoi(ele[1]);
+            streetob.name = ele[2];
+            streetob.length = stoi(ele[3]);
+            c++;
+            streetmap[streetob.name] = streetob;
         }
-        cout << s << "\n";
+        else
+        {
+            cars carob;
+            ll nopath;
+            nopath = stoi(ele[0]);
+            for (ll i = 0; i < nopath; i++)
+            {
+                carob.streetname.push_back(ele[i + 1]);
+                carob.points.push_back(streetmap[ele[i+1]]);
+            }
+            carsvec.push_back(carob);
+        }
     }
     filena.close();
 }
+
+void process_data()
+{
+
+}
+
 int main()
 {
     readfile();
